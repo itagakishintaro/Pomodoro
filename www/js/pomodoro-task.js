2a11c528dab91de7db43427f2354791397e087b6
itagaki <itagaki.shintaro@gmail.com>
2015-02-23 21:33:12 +0900

    version 0.1

--- www/js/pomodoro-task.js
+++ www/js/pomodoro-task.js
@@ -3,6 +3,8 @@
 var todo = [];
 var done = [];
 var max_id = 0;
+var delete_button = '<button class="btn btn-sm btn-default pull-left delete">Delete</button>';
+var finish_button = '<button class="btn btn-sm btn-primary pull-right finish">Finish</button>';
 
 $(function() {
     // clear web storage
@@ -53,32 +55,36 @@ function setTasks() {
 }
 
 function setDragAndDrop() {
-    $('.todo').draggable();
-    $('#drop-here').droppable({
-        drop: function(event, ui) {
-            ui.draggable.children('.finish').click();
-        }
-    });
+    $('.task').draggable();
+    // $('#drop-here').droppable({
+    //     drop: function(event, ui) {
+    //         ui.draggable.children('.finish').click();
+    //     }
+    // });
 }
 
 function setTodoTasks() {
     $('#todo-area').empty();
     todo.forEach(function(element) {
         var id = element.id;
-        var tomatoes = '';
         var task = element.task.replace(/\r?\n/g, '<br>');
-        var finish_button = '<button class="btn btn-primary pull-right finish">Finish</button>';
+        var tomatoes = createTomatoes(element.pomodoro_num);
+        $('#todo-area').append('<div data-id="' + id + '" class="todo task"><div class="content">' + tomatoes + '<br>' + task + '</div>' + delete_button + finish_button + '</div>');
+        setDeleteEvent(element);
+        setFinishEvent(element);
+    });
+}
 
-        for (var i = 0; i < element.pomodoro_num; i++) {
-            tomatoes = tomatoes + '<img src="img/tomato.png" alt="tomato">';
-        }
-        $('#todo-area').append('<div data-id="' + id + '" class="todo task"><div class="content">' + tomatoes + '<br>' + task + '</div>' + finish_button + '</div>');
-        setFinishEvent({
-            id: id,
-            pomodoro_num: element.pomodoro_num,
-            task: element.task,
-            time: Date.now()
+function setDeleteEvent(object) {
+    $('[data-id="' + object.id + '"] .delete').click(function() {
+        todo = todo.filter(function(element) {
+            return (element.id !== object.id);
         });
+        done = done.filter(function(element) {
+            return (element.id !== object.id);
+        });
+        setStorage();
+        setTasks();
     });
 }
 
@@ -87,6 +93,7 @@ function setFinishEvent(object) {
         todo = todo.filter(function(element) {
             return (element.id !== object.id);
         });
+        object.time = Date.now();
         done.push(object);
         setStorage();
         setTasks();
@@ -96,13 +103,18 @@ function setFinishEvent(object) {
 function setDoneTasks() {
     $('#done-area').empty();
     done.forEach(function(element) {
-        var tomatoes = '';
-        for (var i = 0; i < element.pomodoro_num; i++) {
-            tomatoes = tomatoes + '<img src="img/tomato.png" alt="tomato">';
-        }
+        var tomatoes = createTomatoes(element.pomodoro_num);
         var task = element.task.replace(/\r?\n/g, '<br>');
         var date = '<div class="pull-right">' + formatDate(new Date(element.time), 'YYYY/MM/DD hh:mm:ss') + '</div>';
-        $('#done-area').append('<div data-id="' + element.id + '" class="done task"><div class="content">' + tomatoes + '<br>' + task + '</div>' + date + '</div>');
-
+        $('#done-area').append('<div data-id="' + element.id + '" class="done task"><div class="content">' + tomatoes + '<br>' + task + '</div>' + delete_button + date + '</div>');
+        setDeleteEvent(element);
     });
+}
+
+function createTomatoes(number) {
+    var tomatoes = '';
+    for (var i = 0; i < number; i++) {
+        tomatoes = tomatoes + '<img src="img/tomato.png" alt="tomato" class="tomato">';
+    }
+    return tomatoes;
 }
--- www/js/pomodoro-timer.js
+++ www/js/pomodoro-timer.js
@@ -1,35 +1,42 @@
 'use strict';
 
-var STATUS = {WORKING: 'WORKING', RESTING: 'RESTING'};
 var AUDIO_LIST = {
-    ALERT: new Audio('sounds/alert.mp3'),
-    BGM: new Audio('sounds/bgm.mp3')
+    ALERT: 'sounds/alert.mp3',
+    RESTING: 'sounds/bgm.mp3'
 };
-var PERIOD = {WORKING: 5, RESTING:3};
+var STATUS = {
+    WORKING: 'WORKING',
+    RESTING: 'RESTING'
+};
+var PERIOD = {
+    WORKING: 1 * 60,
+    RESTING: 1 * 60
+};
 var OPTIONS = {
     WORKING: {
         easing: 'linear',
         scaleLength: 0,
+        barColor: '#FF9800',
         lineWidth: 10,
         size: 210,
         onStep: function(value) {
-            $(this.el).find('#working-time-view').text( Math.round(value * PERIOD.WORKING / 100) + ' / ' + PERIOD.WORKING + ' min');
+            $(this.el).find('#working-time-view').text(Math.floor(value * PERIOD.WORKING / 100 / 60) + ' / ' + PERIOD.WORKING / 60 + ' min');
         }
     },
     RESTING: {
         easing: 'linear',
         scaleLength: 0,
-        barColor: '#0000FF',
+        barColor: '#4CAF50',
         lineWidth: 10,
         size: 210,
         onStep: function(value) {
-            $(this.el).find('#resting-time-view').text( Math.round(value * PERIOD.RESTING / 100) + ' / ' + PERIOD.RESTING + ' min');
+            $(this.el).find('#resting-time-view').text(Math.floor(value * PERIOD.RESTING / 100 / 60) + ' / ' + PERIOD.RESTING / 60 + ' min');
         }
     }
 };
 var current_status = STATUS.WORKING;
 var intervalID;
-var current_time=0;
+var current_time = 0;
 
 $(function() {
     setupTimer();
@@ -52,7 +59,7 @@ $(function() {
 
 });
 
-function setupTimer(){
+function setupTimer() {
     $('#working-timer').easyPieChart(OPTIONS.WORKING);
     $('#resting-timer').easyPieChart(OPTIONS.RESTING);
     $('#working-timer').hide();
@@ -60,36 +67,44 @@ function setupTimer(){
     getCurrentTimer().show();
 }
 
-function toggleChart(){
+function toggleChart() {
     getCurrentTimer().hide();
     current_status = (current_status === STATUS.WORKING) ? STATUS.RESTING : STATUS.WORKING;
     getCurrentTimer().show();
 }
 
-function startTimer(period){
+function startTimer(period) {
     intervalID = setInterval(updateTimer, 1000);
+    if(current_status === STATUS.RESTING){
+        audioStart(AUDIO_LIST.RESTING);
+    }
 }
 
-function updateTimer(){
+function updateTimer() {
     current_time = current_time + 1;
 
-    getCurrentTimer().data('easyPieChart').update( current_time / PERIOD[current_status] * 100 );
-    if(current_time > PERIOD[current_status]){
+    getCurrentTimer().data('easyPieChart').update(current_time / PERIOD[current_status] * 100);
+    // alert before 5 sec when woking.
+    if (current_status === STATUS.WORKING && current_time === PERIOD[current_status] - 10) {
+        audioStart(AUDIO_LIST.ALERT);
+    }
+    if (current_time > PERIOD[current_status]) {
+        audioStop();
         resetTimer();
         toggleChart();
         startTimer();
     }
 }
 
-function getCurrentTimer(){
+function getCurrentTimer() {
     return (current_status === STATUS.RESTING) ? $('#resting-timer') : $('#working-timer');
 }
 
-function resetTimer(){
+function resetTimer() {
     clearInterval(intervalID);
     current_time = 0;
     $('#working-timer').data('easyPieChart').update(0);
-    $('#working-time-view').text('0 / ' + PERIOD.WORKING + ' min');
+    $('#working-time-view').text('0 / ' + PERIOD.WORKING / 60 + ' min');
     $('#resting-timer').data('easyPieChart').update(0);
-    $('#resting-time-view').text('0 / ' + PERIOD.RESTING + ' min');
+    $('#resting-time-view').text('0 / ' + PERIOD.RESTING / 60 + ' min');
 }
